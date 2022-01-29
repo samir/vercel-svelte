cf4d61e3b8c9624feb5d7c1fae5a7d5e592c07d2
Samir <samir@nextar.com.br>
2022-01-29 14:05:46 -0300

    First commit

---
+++ src/routes/guides/index.svelte
@@ -0,0 +1,49 @@
+<script context="module">
+  export async function load({ fetch }) {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const guides = await res.json()
+
+    if (res.ok) {
+      return {
+        props: {
+          guides
+        }
+      }
+    }
+
+    return {
+      status: res.status,
+      error: new Error('Could not fetch the guides')
+    }
+  }
+</script>
+
+<script>
+  export let guides
+</script>
+
+<div class="guides">
+  <ul>
+    {#each guides as guide}
+      <li>
+        <a sveltekit:prefetch href={`/guides/${guide.id}`}>{guide.title}</a>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  .guides {
+    margin-top: 20px;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  a {
+    display: inline-block;
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px dotted rgba(255,255,255,0.2);
+  }
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+// import netlify from '@sveltejs/adapter-netlify'
+import vercel from '@sveltejs/adapter-vercel'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: vercel(),
+		target: '#svelte'
+	}
+};
+
+export default config;
